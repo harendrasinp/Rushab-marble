76a0da35be4b0be941632cb2f9bacfe216a4f052
Harendrasinh <haren111990@gmail.com>
2025-08-03 22:01:22 +0530

    header images change

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
-
+'use client';
+import React, { useEffect, useState } from 'react';
 
 const Header = () => {
+  const images = [
+    "/Images/headerBackground/bg1.jpg",
+    "/Images/headerBackground/bg2.jpg",
+    "/Images/headerBackground/bg3.jpg",
+    "/Images/headerBackground/bg4.jpg",
+    "/Images/headerBackground/bg5.jpg",
+    "/Images/headerBackground/bg6.jpeg",
+    "/Images/headerBackground/bg7.jpeg"
+  ];
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage(prev => (prev + 1) % images.length);
+    }, 6000); // Change every 4 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <header
-      className="w-full min-h-[12rem] md:h-64 bg-[url('/Images/headerBackground/bg3.jpg')] bg-cover bg-center flex items-start justify-center shadow-lg shadow-gray-800">
-
+      className="w-full min-h-[12rem] md:h-64 bg-cover bg-center flex items-start justify-center shadow-lg shadow-gray-800 transition-all duration-1000"
+      style={{ backgroundImage: `url(${images[currentImage]})` }}
+    >
       <div className='mt-5 flex flex-col items-center justify-center text-center gap-15 w-full px-2'>
-        <img src="/Images/Logo/logo.png" alt="log" className='w-[6rem] h-[5rem] md:w-[9rem] md:h-[8rem] opacity-60' />
-
+        <img
+          src="/Images/Logo/logo.png"
+          alt="logo"
+          className='w-[6rem] h-[5rem] md:w-[9rem] md:h-[8rem] opacity-60'
+        />
         <p className='text-white/70 text-2xl md:text-4xl'>
-          Primium Marble for Every Space
+          Premium Marble for Every Space
         </p>
       </div>
     </header>
   );
-}
+};
 
-export default Header
+export default Header;
