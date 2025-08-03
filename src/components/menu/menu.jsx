77b8b94c0d443ca68bc983993ac6303692224250
Harendrasinh <haren111990@gmail.com>
2025-08-03 21:25:16 +0530

    did responsive menu

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,27 +1,15 @@
 import React from 'react'
-
+import { menuData } from '@/contents/menu/data'
 const Menu = () => {
     return (
-        <div className='mt-10 w-full h-fit bg-gray-100 flex flex-row flex-wrap items-center justify-center gap-3 sm:gap-10 px-2'>
-            <div className='w-[5.5rem] h-[5.5rem] sm:w-[15rem] sm:h-[12rem] flex flex-col items-center justify-center gap-2 hover:scale-105 transition duration-500 cursor-pointer'>
-                <img src="/Images/menu/sink.png" alt="sink" className='w-[4.5rem] h-[4.5rem] sm:w-full sm:h-full shadow-2xl shadow-amber-900 rounded-[0.2rem]' />
-                <div className='text-gray-800 font-semibold text-xs sm:text-base text-center sm:text-left'>Sink</div>
+        <div className='mt-10 w-full h-fit bg-gray-100 flex flex-row flex-wrap items-center justify-center gap-4 sm:gap-10 p-2 sm:p-5'>
+          {menuData.map((item, index) => (
+            <div key={index} className='w-[7rem] h-[7rem] sm:w-[12.5rem] sm:h-[12.5rem] flex flex-col items-center justify-center gap-2 hover:scale-105 transition duration-500 cursor-pointer'>
+              <img src={item.image} alt={item.title} className='w-full h-[80%] shadow-2xl shadow-amber-900 rounded-[0.2rem]' />      
+                <div className='text-gray-800 font-semibold text-xs sm:text-base text-center sm:text-left'>{item.title}</div>   
             </div>
-            <div className='w-[5.5rem] h-[5.5rem] sm:w-[15rem] sm:h-[12rem] flex flex-col items-center justify-center gap-2 hover:scale-105 transition duration-500 cursor-pointer'>
-                <img src="/Images/menu/bathtub.png" alt="sink" className='w-[4.5rem] h-[4.5rem] sm:w-full sm:h-full shadow-2xl shadow-amber-900 rounded-[0.2rem]' />
-                <div className='text-gray-800 font-semibold text-xs sm:text-base text-center sm:text-left'>BathTub</div>
-            </div>
-            <div className='w-[5.5rem] h-[5.5rem] sm:w-[15rem] sm:h-[12rem] flex flex-col items-center justify-center gap-2 hover:scale-105 transition duration-500 cursor-pointer'>
-                <img src="/Images/menu/tilse.png" alt="sink" className='w-[4.5rem] h-[4.5rem] sm:w-full sm:h-full shadow-2xl shadow-amber-900 rounded-[0.2rem]' />
-                <div className='text-gray-800 font-semibold text-xs sm:text-base text-center sm:text-left'>tilse</div>
-            </div>
-            <div className='w-[5.5rem] h-[5.5rem] sm:w-[15rem] sm:h-[12rem] flex flex-col items-center justify-center gap-2 hover:scale-105 transition duration-500 cursor-pointer'>
-                <img src="/Images/menu/slab2.jpg" alt="sink" className='w-[4.5rem] h-[4.5rem] sm:w-full sm:h-full shadow-2xl shadow-amber-900 rounded-[0.2rem]' />
-                <div className='text-gray-800 font-semibold text-xs sm:text-base text-center sm:text-left'>Slab</div>
-            </div>
+          ))}
         </div>
-
     )
 }
-
 export default Menu
---
+++ src/contents/menu/data.js
@@ -0,0 +1,22 @@
+export const menuData = [
+  {
+    id: 1,
+    image: "/Images/menu/sink.png",
+    title: "Sink",
+  },
+  {
+    id: 2,  
+    image: "/Images/menu/bathtub.png",
+    title: "BathTub",
+  },
+    {  
+    id: 3,
+    image: "/Images/menu/tilse.png",
+    title: "Tile",
+  },
+    {   
+    id: 4,
+    image: "/Images/menu/slab2.jpg",
+    title: "Slab",
+  },
+];  
